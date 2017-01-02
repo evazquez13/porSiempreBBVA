0094ef51d8232e3c554773293168478e12812397
Emmanuel <evazquezbravo13@gmail.com>
2017-01-02 16:24:51 -0600

    Se crea pagina preguntas frecuentes y se da estilos en custom y script.js

--- js/script.js
+++ js/script.js
@@ -27,4 +27,19 @@ $('span').on('mouseenter' , function(){
 $('span').on('mouseleave' , function(){
 	$(this).popover('hide')
 })
+$('.collapse')
+		.on('shown.bs.collapse', function() {
+             $(this)
+                 .parent()
+                 .find(".glyphicon-chevron-down")
+                 .removeClass("glyphicon-chevron-down")
+                 .addClass("glyphicon-chevron-up");
+             })
+         .on('hidden.bs.collapse', function() {
+             $(this)
+                 .parent()
+                 .find(".glyphicon-chevron-up")
+                 .removeClass("glyphicon-chevron-up")
+                 .addClass("glyphicon-chevron-down");
+             });
 });
